e7cee8500248f1e7c53c4352cccd982dbb7564d7
Christopher Gutteridge <totl@soton.ac.uk>
2023-11-10 13:14:15 +0000

    revisions based on code review moved the lint ignore lines to only wrap the relevant lines

--- test/Helpers.test.js
+++ test/Helpers.test.js
@@ -6,10 +6,12 @@ describe('Helpers', () => {
     test('When passed an array: return the array unchanged', () => {
       expect(Helpers.ensureArray([1, 2, 3])).toStrictEqual([1, 2, 3])
     })
+
     test('When passed a non array: return it wrapped in an array', () => {
       expect(Helpers.ensureArray(242)).toStrictEqual([242])
     })
   })
+
   describe('tableToRecords()', () => {
     test('When passed a table representing a spreadsheet: Return an array of objects representing the rows as records based on  headings in the first line', () => {
       const records = Helpers.tableToRecords([
